8d793e39b3124c79d827abdc212d11dc3124e193
TristanLemire <tristan.lemire.sio@gmail.com>
2018-12-19 09:49:34 +0100

    home music done

--- src/js/script.js
+++ src/js/script.js
@@ -31,8 +31,7 @@ function startGame() {
     clickMusic.play();
     setTimeout(function() {
       oxo.screens.loadScreen("game", function() {
-        var gameSound = document.getElementById("gameSound");
-        gameSound.play();
+        
         oxo.inputs.cancelKeyListener("space");
         //object joueur1 et 2
         var player1 = { name: "player 1", score: 0 };
